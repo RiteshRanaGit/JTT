bb88504b6a571d6384003b835fdeca683e626688
Ritesh <rana1997ritesh@gmail.com>
2020-09-18 06:05:44 +0530

    fome/ date/ whatsapp first itration

--- js/index.js
+++ js/index.js
@@ -15,38 +15,88 @@ hedading(x);
 function choises(element) {
 
 
-    var strUser = element.value;
-    if (strUser === "Tour") {
-        console.log("inside tour");
+    var choisesName = element.value;
+    function place(strUser){
+        if (strUser === "Tour") {
+        
 
 
-        document.getElementById("tourPlace").classList.remove("hide");
-        document.getElementById("tourPlace").classList.add("show");
-
-        document.getElementById("fromTo").classList.remove("show");
-        document.getElementById("fromTo").classList.add("hide");
-
-        console.log("inside tour 4");
-
-    } else {
-        console.log("inside else tour1");
-        document.getElementById("tourPlace").classList.remove("show");
-        console.log("inside else tour 2");
-        document.getElementById("tourPlace").classList.add("hide");
-        if (strUser === "One way" || strUser === "Two way") {
-            console.log("inside else tour if 1");
-            document.getElementById("fromTo").classList.remove("hide");
-            console.log("inside else tour if 1");
-            document.getElementById("fromTo").classList.add("show");
-        } else {
-
+            document.getElementById("tourPlace").classList.remove("hide");
+            document.getElementById("tourPlace").classList.add("show");
+    
             document.getElementById("fromTo").classList.remove("show");
-
             document.getElementById("fromTo").classList.add("hide");
+    
+            
+    
+            
+    
+        } else {
+            
+            
+            document.getElementById("tourPlace").classList.remove("show"); 
+            document.getElementById("tourPlace").classList.add("hide");
+    
+            if (strUser === "One way" || strUser === "Two way") {
+                
+                document.getElementById("fromTo").classList.remove("hide");
+                document.getElementById("fromTo").classList.add("show");
+    
+                
+    
+            } else {
+    
+                document.getElementById("fromTo").classList.remove("show");
+    
+                document.getElementById("fromTo").classList.add("hide");
+            }
         }
     }
+    place(choisesName);
+
+    function dateTogle(strUser){
+        if (strUser === "One way") {
+        
+
+
+            document.getElementById("pick-date").classList.remove("hide");
+            document.getElementById("pick-date").classList.add("show");
+    
+            document.getElementById("pick-drop-date").classList.remove("show");
+            document.getElementById("pick-drop-date").classList.add("hide");
+    
+            
+    
+            
+    
+        } else {
+            
+            document.getElementById("pick-date").classList.remove("show");
+            document.getElementById("pick-date").classList.add("hide");
+
+            
+    
+            if (strUser === "Tour" || strUser === "Two way") {
+                
+                document.getElementById("pick-drop-date").classList.remove("hide");
+                document.getElementById("pick-drop-date").classList.add("show");
+    
+                
+    
+            } else {
+    
+                document.getElementById("pick-drop-date").classList.remove("show");
+                document.getElementById("pick-drop-date").classList.add("hide");
+            }
+        }
+    }
+    dateTogle(choisesName);
 }
 
-$(document).on('click', '.modal', function(){
-    choises();
-    });
+
+
+        // document.getElementById("pick-drop-date").classList.remove("hide");
+        // document.getElementById("pick-drop-date").classList.add("show");
+
+        // document.getElementById("pick-drop-date").classList.remove("show");
+        // document.getElementById("pick-drop-date").classList.add("hide");
